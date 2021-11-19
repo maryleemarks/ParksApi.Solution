de969ad7f216bc6633f990e9231e384e274ff299
Mary Marks <maryleemarks@gmail.com>
2021-11-18 16:09:21 -0800

    add park model

---
+++ ParkApi/Models/Park.cs
@@ -0,0 +1,12 @@
+namespace ParkApi.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public int Rating { get; set; }
+  }
+}
